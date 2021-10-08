05dbedfa9939f1ecdbf8d298b59cb2655beb695b
muhamad-fadla <yuratamma@gmail.com>
2021-10-08 16:17:23 +0700

    update: resolve issue PORT

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 3001;
-
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
