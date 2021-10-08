9008027459883e13be53a13d5adc3bd0661aee05
muhamad-fadla <yuratamma@gmail.com>
2021-10-08 16:49:17 +0700

    update: resolve issue landing page

--- api/index.js
+++ api/index.js
@@ -2,10 +2,24 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const path = require('path')
 const router = jsonServer.router(path.join(__dirname, '/../db/i.json'));
-const middlewares = jsonServer.defaults();
+const compression = require('compression');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+server.use(cors({ origin: true, credentials: true }))
+server.use([
+		bodyParser.json({ limit: '10mb', extended: false }),
+  		bodyParser.urlencoded({ extended: false }),
+	]);
+server.use(compression())
 
-server.use(middlewares);
+server.use((req, res, next) => {
+      if (req.method === 'GET') {
+        next() // Continue
+      } else {
+        res.sendStatus(403) // Forbidden
+      }
+    });
 server.use(router);
 
 const port = process.env.PORT || 3000;
